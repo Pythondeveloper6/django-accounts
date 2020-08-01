9055e02c03a0265b5531339e2ab3f9ea20c72412
Pythondeveloper6 <pythondeveloper6@gmail.com>
2020-08-02 00:09:55 +0200

    --- Auto-Git Commit ---

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+
+app_name='accounts'
+
 urlpatterns = [
-    path('signup',views.signup , name='signup')
+    path('signup',views.signup , name='signup'),
+    path('profile',views.profile , name='profile'),
+    path('profile/edit',views.profile_edit , name='profile_edit'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from .models import Profile
-from .forms import SignupForm
-
+from .forms import SignupForm , UserForm , ProfileForm
+from django.contrib.auth import authenticate , login
 # Create your views here.
 
 
 
 def signup(request):
-    pass
+    if request.method == 'POST':  ## save
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request,user)
+            return redirect('/accounts/profile')
+
+    else: ## show form
+        form = SignupForm()
+
+    return render(request,'registration/signup.html',{'form':form})
+
+
+def profile(request):
+    profile = Profile.objects.get(user=request.user)
+    return render(request,'profile/profile.html',{'profile':profile})
+
+
+
+
+def profile_edit(request):
+    if request.method == 'POST':
+        userform = UserForm(request.POST)
+        profile_form = ProfileForm(request.POST)
+
+    else:  ## show
+        userform = UserForm()
+        profile_form = ProfileForm()
+
+    return render(request,'profile/profile_edit.html',{
+        'userform' : userform , 
+        'profileform' : profile_form ,
+        })
