1900c4fa026820e1aa0f442b0df2ecc24a4639d8
Pythondeveloper6 <pythondeveloper6@gmail.com>
2020-08-01 23:08:34 +0200

    add profile model to the base user

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+''' 
+    username
+    password 
+    first_name
+    last_name
+    email  
+     '''
+
+class Profile(models.Model):
+    user = models.OneToOneField(User , on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15,null=True , blank=True)
+    addres = models.CharField(max_length=50 , blank=True, null=True)
+    # image
+    # age
+    # Job
+
+    def __str__(self):
+        return str(self.user)
