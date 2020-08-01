703dd0a1fb4e391586ff2901d2a68e34b2442056
Pythondeveloper6 <pythondeveloper6@gmail.com>
2020-08-02 00:50:17 +0200

    add login with email or password

--- accounts/backends.py
+++ accounts/backends.py
@@ -5,26 +5,30 @@ from django.db.models import Q
 
 class EmailBackend(ModelBackend):
     def authenticate(self,request,username=None,password=None,**kwargs):
+        ''' check username or email & password is a valid user'''
         try:
             user = User.objects.get(
                 Q(username__iexact=username) | 
                 Q(email__iexact=username)
             )
         except User.DoesNotExist:
-            pass
+            return None
+
         except MultipleObjectsReturned:
             return User.objects.filter(email=username).order_by('id').first()
 
         else:
-            if user.check_password(pasword) and self.user.can_authenticate(user):
+            if user.check_password(password) and self.user_can_authenticate(user):
                 return user
 
 
     def get_user(self,user_id):
         try :
             user = User.objects.get(pk=user_id)
-            
+
         except User.DoesNotExist:
-            pass  
+            return None
+
+        return user if self.user_can_authenticate(user) else None
 
         
--- project/settings.py
+++ project/settings.py
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/accounts/profile'
+
+AUTHENTICATION_BACKENDS = ['accounts.backends.EmailBackend']
